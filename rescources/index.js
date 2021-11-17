dd3289900e8a582348ac73a0b53104465c285b24
MetallicWeb <64267884+bensonchow123@users.noreply.github.com>
2021-11-17 20:43:06 +0000

    Update index.js

--- rescources/index.js
+++ rescources/index.js
@@ -1,1 +1,8 @@
-
+function addFace() {
+  var img = document.createElement("img");
+  img.src = "rescources/media/face-spinning.gif";
+  img.classList.add('box2d');
+  img.setAttribute("id", "smallFace");
+  body = document.getElementsByTagName('body')[0];
+  body.appendChild(img)
+}

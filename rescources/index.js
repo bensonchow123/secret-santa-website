88a1d38686800388a9b02ffcbc972c875c349208
MetallicWeb <64267884+bensonchow123@users.noreply.github.com>
2021-11-20 17:31:26 +0000

    Update index.js

--- rescources/index.js
+++ rescources/index.js
@@ -1,8 +1,11 @@
-function addFace() {
-  var img = document.createElement("img");
-  img.src = "rescources/media/face-spinning.gif";
+function randomImage(){
+  let imagesArray = ['media/face-spinning.gif', 'media/snowman.png'];
+  let num = Math.floor(Math.random() * imagesArray.length);
+  let randomNum = Math.random() * (11 - 6 + 1) + 6;
+  let img = document.createElement("img");
+  image.src = imagesArray[num];
   img.classList.add('box2d');
-  img.setAttribute("id", "smallFace");
+  image.style.height = `${randomNum}vmin`;
   div = document.getElementById("top");
   div.appendChild(img)
   init()

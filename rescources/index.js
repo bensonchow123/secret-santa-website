423e2794629e337a72ffb78386b1037c01bc5ecb
MetallicWeb <64267884+bensonchow123@users.noreply.github.com>
2021-11-27 22:03:46 +0000

    Update index.js

--- rescources/index.js
+++ rescources/index.js
@@ -1,5 +1,3 @@
-let clickNum  = 0;
-let randomClick =  Math.floor(Math.random() * (20 - 10 + 1) + 10);
 function randomImage(){
   let imagesArray = ['rescources/media/face-spinning.gif', 'rescources/media/snowman.png', 'rescources/media/elves.png'];
   let num = Math.floor(Math.random() * imagesArray.length);
@@ -11,29 +9,4 @@ function randomImage(){
   div = document.getElementById("clickme");
   div.appendChild(img)
   init()
-  clickNum += 1
-  if (clickNum % randomClick === 0){
-    randomJoke()
-    randomClick =  Math.floor(Math.random() * (20 - 10 + 1) + 10);
-    clickNum = 0;
-  }
-}
-
-function randomJoke (){
-  let randomJokes = ['Why do reindeer like Beyoncé so much? She sleighs.',
-                     'What reindeer game do reindeer play at sleepovers? Truth or deer.',
-                     'What did Santa say when he stepped into a big puddle? It must have reindeer.',
-                     'What does Rudolph want for Christmas? A Pony sleigh station.',
-                     'What is Santas dogs name? Santa Paws!',
-                     'Where do Santas reindeer stop for coffee? Star-bucks!',
-                     'What’s every elf’s favorite type of music? Wrap!',
-                     'What’s every elf’s favorite type of music? Wrap!',
-                     'What happens if you eat Christmas decorations? You get tinsel-it is'
-                    ];
-  let index = Math.floor(Math.random() * randomJokes.length);
-  alert(randomJokes[index])
-}
-
-function onload() {
-  alert("Merry christmas!!!");
 }

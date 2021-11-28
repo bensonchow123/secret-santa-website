6e5b6ee61794dd7c1e63c175b3a9834f5a7ce444
MetallicWeb <64267884+bensonchow123@users.noreply.github.com>
2021-11-28 12:30:32 +0000

    Update index.js

--- rescources/index.js
+++ rescources/index.js
@@ -1,3 +1,5 @@
+let clickNum  = 0;
+let randomClick =  Math.floor(Math.random() * (20 - 10 + 1) + 10);
 function randomImage(){
   let imagesArray = ['rescources/media/face-spinning.gif', 'rescources/media/snowman.png', 'rescources/media/elves.png'];
   let num = Math.floor(Math.random() * imagesArray.length);
@@ -9,4 +11,27 @@ function randomImage(){
   div = document.getElementById("clickme");
   div.appendChild(img)
   init()
+  clickNum += 1
+  if (clickNum % randomClick === 0){
+    randomJoke()
+    randomClick = Math.floor(Math.random() * (20 - 10 + 1) + 10);
+    clickNum = 0;
+  }
+}
+
+function randomJoke() {
+  let popup = document.getElementById("Popup");
+  let randomJokes = ['Why do reindeer like Beyoncé so much? She sleighs.',
+                     'What reindeer game do reindeer play at sleepovers? Truth or deer.',
+                     'What did Santa say when he stepped into a big puddle? It must have reindeer.',
+                     'What does Rudolph want for Christmas? A Pony sleigh station.',
+                     'What is Santas dogs name? Santa Paws!',
+                     'Where do Santas reindeer stop for coffee? Star-bucks!',
+                     'What’s every elf’s favorite type of music? Wrap!',
+                     'What’s every elf’s favorite type of music? Wrap!',
+                     'What happens if you eat Christmas decorations? You get tinsel-it is'
+                    ];
+  let index = Math.floor(Math.random() * randomJokes.length);
+  popup.innerHTML = randomJokes[index];
+  popup.classList.toggle("show");
 }

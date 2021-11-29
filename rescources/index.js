4ee27a2ed9fbf499e19237c5a9b4324f1cc1a10c
MetallicWeb <64267884+bensonchow123@users.noreply.github.com>
2021-11-29 20:31:05 +0000

    Update index.js

--- rescources/index.js
+++ rescources/index.js
@@ -1,3 +1,6 @@
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 let clickNum  = 0;
 let randomClick =  Math.floor(Math.random() * (20 - 10 + 1) + 10);
 function randomImage(){
@@ -18,32 +21,38 @@ function randomImage(){
     clickNum = 0;
   }
 }
-
+let running = false;
 function randomJoke() {
-  let popup = document.getElementById("myPopup");
-  let randomJokes = ['Why do reindeer like Beyoncé so much? She sleighs.',
-                     'What reindeer game do reindeer play at sleepovers? Truth or deer.',
-                     'What did Santa say when he stepped into a big puddle? It must have reindeer.',
-                     'What does Rudolph want for Christmas? A Pony sleigh station.',
-                     'What is Santas dogs name? Santa Paws!',
-                     'Where do Santas reindeer stop for coffee? Star-bucks!',
-                     'What’s every elf’s favorite type of music? Wrap!',
-                     'What happens if you eat Christmas decorations? You get tinsel-it is',
-                     'What do snowmen wear on their heads? Ice caps!',
-                     'What did Adam say the day before Christmas? “It’s Christmas, Eve!”',
-                     'What do you get when you cross a snowman with a vampire? Frostbite!',
-                     'What did the stamp say to the Christmas card? Stick with me and we’ll go places!',
-                     'Why did no one bid for Rudolph and Blitzen on eBay? Because they were two deer!',
-                     'What does the Queen call her Christmas Broadcast? The One Show!',
-                     'Why don’t you ever see Santa in hospital? Because he has private elf care!',
-                     'How did Mary and Joseph know Jesus’ weight when he was born? They had a weigh in a manger!',
-                     'Why is it getting harder to buy Advent calendars? Their days are numbered!',
-                     'How did Scrooge win the football game? The ghost of Christmas passed!',
-                     'What do angry mice send to each other at Christmas? Cross-mouse cards!',
-                     'What do you call a bunch of chess players bragging about their games in a hotel lobby? Chess nuts boasting in an open foyer!'
-                    ];
-  
-  let index = Math.floor(Math.random() * randomJokes.length);
-  popup.innerHTML = randomJokes[index];
-  popup.classList.toggle("show");
+  if (!running) {
+    running = true;
+    let popup = document.getElementById("myPopup");
+    let randomJokes = ['Why do reindeer like Beyoncé so much? She sleighs.',
+                       'What reindeer game do reindeer play at sleepovers? Truth or deer.',
+                       'What did Santa say when he stepped into a big puddle? It must have reindeer.',
+                       'What does Rudolph want for Christmas? A Pony sleigh station.',
+                       'What is Santas dogs name? Santa Paws!',
+                       'Where do Santas reindeer stop for coffee? Star-bucks!',
+                       'What’s every elf’s favorite type of music? Wrap!',
+                       'What happens if you eat Christmas decorations? You get tinsel-it is',
+                       'What do snowmen wear on their heads? Ice caps!',
+                       'What did Adam say the day before Christmas? “It’s Christmas, Eve!”',
+                       'What do you get when you cross a snowman with a vampire? Frostbite!',
+                       'What did the stamp say to the Christmas card? Stick with me and we’ll go places!',
+                       'Why did no one bid for Rudolph and Blitzen on eBay? Because they were two deer!',
+                       'What does the Queen call her Christmas Broadcast? The One Show!',
+                       'Why don’t you ever see Santa in hospital? Because he has private elf care!',
+                       'How did Mary and Joseph know Jesus’ weight when he was born? They had a weigh in a manger!',
+                       'Why is it getting harder to buy Advent calendars? Their days are numbered!',
+                       'How did Scrooge win the football game? The ghost of Christmas passed!',
+                       'What do angry mice send to each other at Christmas? Cross-mouse cards!',
+                       'What do you call a bunch of chess players bragging about their games in a hotel lobby? Chess nuts boasting in an open foyer!'
+                      ];
+
+    let index = Math.floor(Math.random() * randomJokes.length);
+    popup.innerHTML = randomJokes[index];
+    popup.classList.toggle("show");
+    sleep(4000)
+    popup.classList.toggle("show");
+    running = false;
+  }
 }
